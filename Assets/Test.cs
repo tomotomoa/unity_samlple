fb12b50db1fefd3ac360cf1aafbd808729446f55
imaze <tttttttt652@gmail.com>
2020-05-04 23:36:55 +0900

    Testプロジェクト

---
+++ Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Vector3 startpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpos =  GetComponent<Transform> ().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform tr = GetComponent<Transform> ();
+
+        Vector3 pos = new Vector3 (
+                        startpos.x + Random.Range(-2.0f, 2.0f),
+                        startpos.y + Random.Range(-2.0f, 2.0f)
+        );
+        tr.position = pos;
+    }
+}
